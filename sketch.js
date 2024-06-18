1d212e55de604501aa755737c497d044d9066b0d
Jhane-Vivian <107698971+Jhane-Vivian@users.noreply.github.com>
2024-06-18 20:49:12 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,26 +5,29 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(windowWidth,windowHeight);
-    background(0);
     player = createSprite(10,50,6,20);
     
 }
 
 function draw(){
+    background(0);
     
     if(keyIsDown(65) || keyIsDown(LEFT_ARROW)){
         console.log("Movendo para esquerda!");
-        player.x -=2;
+        player.position.x -=2;
+        
     }
     if(keyIsDown(68) || keyIsDown(RIGHT_ARROW)){
         console.log("Movendo para direita!")
-        player.x +=2;
+       player.position.x +=2;
     }
     if(keyIsDown(83) || keyIsDown(DOWN_ARROW)){
-        console.log("Movendo para baixo!")
+        console.log("Movendo para baixo!");
+        player.position.y+=2;
     }
     if(keyIsDown(87) || keyIsDown(UP_ARROW)){
         console.log("Movendo para cima!")
+        player.position.y-=2;
     }
     drawSprites();  
 }
